caef59f6cad1f75a3d5cf0b92fc15cecccf908e6
venky27011995 <venkatesh.jmails@gmail.com>
2020-10-19 19:37:17 +0530

    Added date

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [amount, setAmount] = useState(1)
   const [exchangeRate, setExchangeRate] = useState()
   const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  const [date, setDate] = useState()
   console.log(2 * exchangeRate)
 
   let fromAmount, toAmount
@@ -33,10 +34,12 @@ function App() {
         setFromCurrency(data.base);
         setToCurrency(firstCurrency);
         setExchangeRate(data.rates[firstCurrency]);
+        setDate(data.date)
       })
 
   }, []);
 
+  // useEffect hook wil call when user made change of currency type fields
   useEffect(() => {
     if (fromCurrency != null && toCurrency != null) {
       fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
@@ -45,27 +48,26 @@ function App() {
           setExchangeRate(data.rates[toCurrency]);
         })
     }
-
   }, [fromCurrency, toCurrency])
 
   function onChangeFromAmount(e) {
     fromAmount = e.target.value;
     setAmount(fromAmount);
     setAmountInFromCurrency(true)
-
   }
+
   function onChangeToAmount(e) {
     toAmount = e.target.value;
     setAmount(toAmount);
     setAmountInFromCurrency(false)
-
   }
 
 
   return (
     <div className="app">
       <div className="app_bodyContainer">
-        <h1>Currency Converter</h1>
+        <h2>Currency Converter</h2>
+        <p><strong>Date:</strong><span>{date}</span></p>
         <Converter
           currencyOptions={currencyOptions}
           selectedCurrency={fromCurrency}
@@ -73,7 +75,8 @@ function App() {
           onChangeCurrency={(e) => setFromCurrency(e.target.value)}
           onChangeAmount={onChangeFromAmount}
         />
-        <h3 className='text-center'>=</h3>
+        <h3 className='text-center'></h3>
+        <img src="https://www.freeiconspng.com/uploads/arrow-up-icon-29.png" className="img-fluid" className='w-25' />
         <Converter
           currencyOptions={currencyOptions}
           selectedCurrency={toCurrency}
